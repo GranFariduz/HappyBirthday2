2806464aa6e37d6700a40d77a174ecd3fdc7f9b2
Farid <relampagolanzadel04@gmail.com>
2018-06-04 15:38:18 +0530

    added loader animation

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 //Variables
 const loaderDiv = document.querySelector('.loader');
+const loader = document.querySelector('.loader div');
+
 let width = window.innerWidth;
 let height = window.innerHeight;
 
 const header = document.querySelector('header');
+
+loader.style.margin = `${height/2.5}px auto`;
 
 loaderDiv.style.width = width + 'px';
 loaderDiv.style.height = height + 'px';
@@ -13,6 +17,7 @@ body.onload = () => {
 
     loaderDiv.style.transform = `translateY(-${height}px)`;
     loaderDiv.style.opacity = `0`;
+    body.style.overflow = 'auto';
 
     header.style.height = `${height}px`;
 
