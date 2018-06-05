e36d7e51c1e4f722fe36860980d6ad1a238e54bd
Farid <relampagolanzadel04@gmail.com>
2018-06-05 16:39:49 +0530

    Added navbar and blurred header image

--- js/index.js
+++ js/index.js
@@ -7,9 +7,12 @@ let height = window.innerHeight;
 
 const header = document.querySelector('header');
 
+
+//Setting the loader's size to that of the user's screen
 loaderDiv.style.width = width + 'px';
 loaderDiv.style.height = height + 'px';
 
+//when the body is loaded, remove the loader and set the header's size to that of the user's screen
 const body = document.querySelector('body');
 body.onload = () => {
 
@@ -17,6 +20,6 @@ body.onload = () => {
     loaderDiv.style.opacity = `0`;
     body.style.overflow = 'auto';
 
-    header.style.height = `${height}px`;
+    header.style.height = `${height -  100}px`;
 
 }
