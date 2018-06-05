453ba67f141f802cc8e29b83f1f67d9f6002a9f6
Farid <relampagolanzadel04@gmail.com>
2018-06-05 17:18:10 +0530

    Parallax scrolling added

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 const loaderDiv = document.querySelector('.loader');
 const loader = document.querySelector('.loader div');
 const all = document.querySelector('*');
+const headerBox = document.querySelector('.header-box');
 
 let width = window.innerWidth;
 let height = window.innerHeight;
@@ -25,3 +26,11 @@ body.onload = () => {
     header.style.height = `${height -  100}px`;
 
 }
+
+//For parallax scrolling
+window.onscroll = function() {
+
+    let scrollVal = this.pageYOffset;
+    headerBox.style.transform = `translateY(-${scrollVal/2}px)`;
+
+};
